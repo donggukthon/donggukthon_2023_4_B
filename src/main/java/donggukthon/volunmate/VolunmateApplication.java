f98db1ccb8e1c2313d6a1a9194d468e7a8eb9834
kurtyoon <akakertcobain@naver.com>
2023-12-18 00:23:33 +0900

    [Feat] 프로젝트를 init한다.

---
+++ src/main/java/donggukthon/volunmate/VolunmateApplication.java
@@ -0,0 +1,13 @@
+package donggukthon.volunmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolunmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VolunmateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/donggukthon/volunmate/VolunmateApplicationTests.java
@@ -0,0 +1,13 @@
+package donggukthon.volunmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolunmateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
